6b11baa3a197e11443b20a4958b889859532a3e1
Caiden-Heinrichs <cjheinrichs@students.unwsp.edu>
2026-02-13 17:14:23 -0600

    Completed program_5.py

--- program_5.py
+++ program_5.py
@@ -1,19 +1,31 @@
-# Program #5: Bank Balance
-# Write a program that asks the user to enter the amount that he or she has budgeted for a month.
-# A loop should then prompt the user to enter each of his or her expenses for the 
-# month and keep a running total. (Enter 0 to exit the loop)  
-# When the loop finishes, the program should display the amount that the 
-# user is over or under budget.
+#Week 4, Program 5 - Bank Balance
+#Caiden Heinrichs
+#02/13/26
 
 def main():
-    budget = 0.0
-    difference = 0.0
-    spent = 1.0         #initialize for while loop
-    total = 0.0
+    #Set up variables for tracking
+    budget = float(input("How much did you budget for this month? $"))
+    totalSpending = 0.0
 
-    ######################
-    # WRITE YOUR CODE HERE
-    ######################
+    while True:
+        #Get the expense
+        expense = float(input("Enter the cost of the expense (0 to exit): $"))
+
+        #Check if the user ends the loop
+        if expense == 0:
+            break
+        else:
+            #Update total spending counter
+            totalSpending += expense
+
+    #Calculate the amount over/under budget
+    difference = budget - totalSpending
+
+    #Print the amount over/under budget
+    if difference >= 0:
+        print("You are $", round(difference, 2), "under your budget.")
+    else:
+        print("You are $", round(-difference, 2), "over your budget!")
 
 
 if __name__ == '__main__':
